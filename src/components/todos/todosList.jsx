792c6b029df1d894d8136f03cfb025fd2fcad250
nicoliprogramer <imotivacionesf@gmail.com>
2023-08-23 16:21:58 -0300

    add option of share

--- src/components/todos/todosList.jsx
+++ src/components/todos/todosList.jsx
@@ -5,6 +5,9 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import Checkbox from "@mui/material/Checkbox";
 import { Grid, Typography } from "@mui/material";
+import IconButton from '@mui/material/IconButton';
+import GroupAddSharpIcon from '@mui/icons-material/GroupAddSharp';
+import ListItemIcon from '@mui/material/ListItemIcon';
 
 type TodosProps = {
   id: Number,
@@ -24,34 +27,35 @@ export const TodosList: FC<TodosProps> = ({
 
   return (
         <>
-        <List
-          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper"}}
-        >
-           <ListItem
-           alignItems="center"
-             key={id}
-             secondaryAction={
-               <Checkbox
-                 edge="end"
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+          <ListItem
+            key={id}
+            secondaryAction={
+              <IconButton edge="end">
+                <GroupAddSharpIcon />
+              </IconButton>
+            }
+            disablePadding
+          >
+            <ListItemButton>
+              
+              <ListItemIcon>
+                <Checkbox
+                   edge="end"
                  inputProps={{ "aria-labelledby": isCompleted }}
-               />
-             }
-             disablePadding
-           >
-             <ListItemButton> 
-               <ListItemText
-                 id={id}
-                 primary={`${text}`}
-               />
-             </ListItemButton>
-           </ListItem>
-             <Grid ml={2}>
-              <Typography variant="caption">
-                      {hour}
-              </Typography>
-              </Grid>
-              </List>
+                />
+                
+              </ListItemIcon>
+              
+              <ListItemText primary={`${text}`} />
+
+            </ListItemButton>
+            
+          </ListItem>
+          <Grid ml={9}>
+          <ListItemText secondary={`${hour}`}/>
+          </Grid>
+        </List>
             </>
-           
   );
 };
